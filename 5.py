273ea7b2a051300c65c02508dcb08ad7f4fc545b
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-04 15:33:40 +0530

    range or function

---
+++ 5.py
@@ -0,0 +1,28 @@
+numbers = list(range(10))
+print(numbers)
+
+numbers = list(range(3,8))
+print(numbers)
+
+numbers = list(range(1,30))
+print(numbers)
+
+numbers = list(range(1,30,3))
+print(numbers)
+
+numbers = list(range(1,30,5))
+print(numbers)
+
+numbers = list(range(1,100,3))
+print(numbers)
+
+
+def function():
+    print("abc")
+    print("apple")
+    print("new york")
+
+function()
+function()
+
+
