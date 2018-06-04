6dc78f583a7ddc245d6d2f11aae745e8e453b568
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-04 15:09:54 +0530

    list

---
+++ 4.py
@@ -0,0 +1,40 @@
+list =["Mike","john","Rob"]
+print(list)
+print(list[0])
+print(list[1])
+print(list[2])
+
+numbers =[1,2,3,4,5]
+print(numbers)
+print(numbers[4])
+
+print(list+numbers)
+
+abc=[]
+print(abc)
+
+number = [1,1,1,1,1]
+number[2] = 5
+print(number)
+
+newnumber = [2,2,2,2,2]
+print(number+newnumber)
+
+print(number*3)
+print(newnumber*3)
+
+fruits = ["apple", "mango", "peach"]
+
+print ("apple" in fruits)
+print ("orange" in fruits)
+
+fruit = ["apple","mango","peach","orange"]
+fruit.append("banana")
+fruit.append("grapes")
+print(fruit)
+print(len(fruit))
+
+fruit.insert(1,"pineapple")
+print(fruit)
+
+print(fruit.index("peach")) 
